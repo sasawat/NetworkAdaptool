d68b850f749101bf089316d1a1dceb97eccd364a
Izumi Konata <sasawat@outlook.com>
2014-10-07 12:26:36 -0400

    Fixing IPV4Settings
    Accidentally tried to serialize NetworkAdapter instead of IPV4Settings.
    Need more sleep.

--- NetworkAdaptool/IPV4Settings.cs
+++ NetworkAdaptool/IPV4Settings.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace NetworkAdaptool
 {
@@ -17,6 +19,15 @@ namespace NetworkAdaptool
         public string strDefaultGateway;
         public string strName;
 
+        /// <summary>
+        /// Creates an IPV4Settings from a bunch of settings for IPV4... 
+        /// </summary>
+        /// <param name="strIpAddr">IPV4 Address in format "x.x.x.x"</param>
+        /// <param name="strSubnetMask">IPV4 Subnet Mask in format "x.x.x.x". NOT CIDR.</param>
+        /// <param name="strDNS1">The preferred DNS server in format "x.x.x.x"</param>
+        /// <param name="strDNS2">The alternate DNS server in format "x.x.x.x"</param>
+        /// <param name="strDefaultGateway">The default gateway in format "x.x.x.x"</param>
+        /// <param name="strName">The name for the profile.</param>
         public IPV4Settings(string strIpAddr, string strSubnetMask, string strDNS1, string strDNS2, string strDefaultGateway, string strName)
         {
             this.strIpAddr = strIpAddr;
@@ -27,6 +38,11 @@ namespace NetworkAdaptool
             this.strName = strName;
         }
 
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         public IPV4Settings(SerializationInfo info, StreamingContext context)
         {
             //Check if right version
@@ -44,6 +60,11 @@ namespace NetworkAdaptool
             strDefaultGateway = (string)info.GetValue("strDefaultGateway", typeof(string));
         }
 
+        /// <summary>
+        /// Serialization method
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             //Serialize ALL the things
@@ -54,6 +75,70 @@ namespace NetworkAdaptool
             info.AddValue("strDNS2", strDNS2, typeof(string));
             info.AddValue("strDefaultGateway", strDefaultGateway, typeof(string));
             info.AddValue("strName", strName, typeof(string));
+        }
+
+        /// <summary>
+        /// Serializes the NetworkAdapter into the stream
+        /// </summary>
+        /// <param name="river">Stream to serialize into</param>
+        public void write(Stream river)
+        {
+            //Make a BinaryFormatter to serialize
+            BinaryFormatter boyfriend = new BinaryFormatter();
+
+            //Serialize into the stream 
+            boyfriend.Serialize(river, this);
+        }
+
+        /// <summary>
+        /// Serializes a Dictionary of NetworkAdapters into the stream
+        /// </summary>
+        /// <param name="river">Stream to serialize into</param>
+        /// <param name="dic">Dictionary of network adapters to serialize</param>
+        public static void writeLibrary(Stream river, Dictionary<string, IPV4Settings> dic)
+        {
+            //Make a BinaryFormatter to serialize
+            BinaryFormatter boyfriend = new BinaryFormatter();
+
+            //Serialize into the stream 
+            boyfriend.Serialize(river, dic);
+        }
+
+        /// <summary>
+        /// Deserializes a NetworkAdapter from a stream
+        /// </summary>
+        /// <param name="river"></param>
+        /// <returns></returns>
+        public static IPV4Settings read(Stream river)
+        {
+            BinaryFormatter boyfriend = new BinaryFormatter();
+
+            try
+            {
+                return (IPV4Settings)(boyfriend.Deserialize(river));
+            }catch(Exception ex)
+            {
+                throw new SerializationException("Stream does not contain IPV4Settings");
+            }
+        }
+
+        /// <summary>
+        /// Deserializes a Dictionary of NetworkAdapters from a stream
+        /// </summary>
+        /// <param name="river"></param>
+        /// <returns></returns>
+        public static Dictionary<string, IPV4Settings> readLibrary(Stream river)
+        {
+            BinaryFormatter boyfriend = new BinaryFormatter();
+
+            try
+            {
+                return (Dictionary<string, IPV4Settings>)(boyfriend.Deserialize(river));
+            }
+            catch (Exception ex)
+            {
+                throw new SerializationException("Stream does not contain Dictionary<string, IPV4Settings>");
+            }
         }
     }
 }
--- NetworkAdaptool/Program.cs
+++ NetworkAdaptool/Program.cs
@@ -8,6 +8,7 @@ namespace NetworkAdaptool
 {
     static class Program
     {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
